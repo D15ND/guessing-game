86a29dbe2ca1b0e8f633c07579a4c456db9ce228
ILYA <106765406+D15ND@users.noreply.github.com>
2023-02-21 21:26:40 +0300

    feat: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        return this.flo=Math.round((this.min+this.max)/2);
     }
 
     lower() {
-
+        return this.max=this.flo;
     }
 
     greater() {
-
+        return this.min=this.flo;
     }
 }
 
